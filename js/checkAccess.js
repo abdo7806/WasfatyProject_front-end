9a606240391d5463eeb4323205161760cf4d6885
Abdulsalam <balzhaby26@gmail.com>
2025-05-14 00:47:07 +0300

    Starting with the patient's access to login and the home page, and viewing all of his prescriptions

--- js/checkAccess.js
+++ js/checkAccess.js
@@ -88,3 +88,36 @@ async function getDactorByUserId() {
 
     }));
 }
+
+
+
+
+async function getPatientByUserId() {
+
+    const userData = JSON.parse(localStorage.getItem("userData"));
+
+    let userId = userData.userId;
+
+    const response = await fetch(`https://localhost:7219/api/PatientController/GetPatientByUserId/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+
+    });
+
+    // إخفاء مؤشر تحميل
+
+
+    if (!response.ok) {
+        const errorData = await response.json().catch(() => ({}));
+        throw new Error(errorData.message || 'البريد الإلكتروني أو كلمة المرور غير صحيحة');
+
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+    localStorage.setItem('patientData', JSON.stringify(data));
+}
